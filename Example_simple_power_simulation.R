277e76538f57d446f497802fb837f33f9261befd
ChristinaHaag <c.haag@psychologie.uzh.ch>
2020-05-23 20:33:12 +0100

    Update Example_simple_power_simulation.R

--- Example_simple_power_simulation.R
+++ Example_simple_power_simulation.R
@@ -5,11 +5,9 @@
 # Set random number generator
 set.seed(1234)
 
+
 # Sample size
 N <- 100
-
-# Variables in the model
-soc.connection     <- round(abs(rnorm(N, 2, sd=5)), digits = 2)    # degree of perceived social connection
 
 
 # Conduct 1000 simulations
@@ -17,8 +15,8 @@ for(i in 1:1000) {
   
              # More wellbeing is linked to a higher degree of perceived social connection (0.4*soc.connection)
              # This effect gets stronger over time (interaction: 0.9*soc.connection*time)
-             soc.connection    <- round(abs(rnorm(N, 2, sd=5)), digits = 2)    # degree of perceived social connection
-             wellbeing         <- 20 + 0.4*soc.connection + rnorm(N, 0, sd = 4)
+             soc.connection    <- round(abs(rnorm(N, 2, sd=5)), digits = 2)       # degree of perceived social connection
+             wellbeing         <- 20 + 0.4*soc.connection + rnorm(N, 0, sd = 4)   # simulate wellbeing from model
              model1            <- lm(wellbeing ~ soc.connection)
              p.value[[i]]      <- summary(model1)$coefficients["soc.connection", "Pr(>|t|)"]
              }

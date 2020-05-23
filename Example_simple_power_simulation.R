3f1d7c9814d762add26d21981f61f259b6f1fa73
ChristinaHaag <c.haag@psychologie.uzh.ch>
2020-05-23 19:23:55 +0100

    Update Example_simple_power_simulation.R

--- Example_simple_power_simulation.R
+++ Example_simple_power_simulation.R
@@ -16,7 +16,7 @@ soc.connection     <- round(abs(rnorm(N, 2, sd=5)), digits = 2)    # degree of p
 for(i in 1:1000) {
   
              # More wellbeing is linked to a higher degree of perceived social connection (0.4*soc.connection)
-    # This effect gets stronger over time (interaction: 0.9*soc.connection*time)
+             # This effect gets stronger over time (interaction: 0.9*soc.connection*time)
              soc.connection    <- round(abs(rnorm(N, 2, sd=5)), digits = 2)    # degree of perceived social connection
              wellbeing         <- 20 + 0.4*soc.connection + rnorm(N, 0, sd = 4)
              model1            <- lm(wellbeing ~ soc.connection)

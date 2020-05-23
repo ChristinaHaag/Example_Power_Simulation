2859baaea43827bc385f4c99802aed27283c401b
ChristinaHaag <c.haag@psychologie.uzh.ch>
2020-05-23 20:33:53 +0100

    Update Example_simple_power_simulation.R

--- Example_simple_power_simulation.R
+++ Example_simple_power_simulation.R
@@ -14,7 +14,6 @@ N <- 100
 for(i in 1:1000) {
   
              # More wellbeing is linked to a higher degree of perceived social connection (0.4*soc.connection)
-             # This effect gets stronger over time (interaction: 0.9*soc.connection*time)
              soc.connection    <- round(abs(rnorm(N, 2, sd=5)), digits = 2)       # degree of perceived social connection
              wellbeing         <- 20 + 0.4*soc.connection + rnorm(N, 0, sd = 4)   # simulate wellbeing from model
              model1            <- lm(wellbeing ~ soc.connection)
@@ -32,6 +31,7 @@ significant  <- ifelse(p.value < 0.05, 1, 0)   # If p-value is < 0.05 recode as 
 # Compute power
 ((sum(significant))/1000)*100 # Amount of significant results divided by number of simulations
                               # multiplied by 100 to obtain percentages
+
 
 # 86.8 % power
 
